27810a39c74f27b02e5dba1e81e369df477f2ffa
Tom L'Hotellier <119570582+TomitoBrees@users.noreply.github.com>
2026-07-29 15:37:45 +0200

    Fix for auth page

--- app/auth/update-password/page.tsx
+++ app/auth/update-password/page.tsx
@@ -1,10 +1,14 @@
+import { Suspense } from 'react'
+
 import { UpdatePasswordForm } from '@/components/login/update-password-form'
 
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <UpdatePasswordForm />
+        <Suspense fallback={null}>
+          <UpdatePasswordForm />
+        </Suspense>
       </div>
     </div>
   )
